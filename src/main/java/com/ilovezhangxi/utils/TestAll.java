5d399cb852ce993fc4417be20fb84d384d170454
DJoel1107 <1060269934@qq.com>
2017-04-17 12:35:57 +0800

    just a test

---
+++ src/main/java/com/ilovezhangxi/utils/TestAll.java
@@ -0,0 +1,10 @@
+package com.ilovezhangxi.utils;
+
+public class TestAll {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Just a test");
+	}
+
+}
